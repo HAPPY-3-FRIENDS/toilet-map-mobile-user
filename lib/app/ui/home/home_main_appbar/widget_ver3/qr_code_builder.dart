a5a4d04cd3d7c39504491aef2fbef49969e4bcb9
qmint <nguyendaoducquan@gmail.com>
2023-03-27 23:26:35 +0700

    [dq-qiji] UI - Home - QR code builder realtime

--- lib/app/ui/home/home_main_appbar/widget_ver3/qr_code_builder.dart
+++ lib/app/ui/home/home_main_appbar/widget_ver3/qr_code_builder.dart
@@ -1,30 +1,66 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:toiletmap/app/utils/constants.dart';
 
-class QRCodeBuilder extends StatelessWidget {
+class QRCodeBuilder extends StatefulWidget {
   final String data;
   final int accountId;
 
-  const QRCodeBuilder({Key? key, required this.data, required this.accountId}) : super(key: key);
+  const QRCodeBuilder({Key? key, required this.data, required this.accountId})
+      : super(key: key);
+
+  @override
+  State<QRCodeBuilder> createState() => _QRCodeBuilderState();
+}
+
+class _QRCodeBuilderState extends State<QRCodeBuilder> {
+  int time = 15;
+  Timer? timer;
+  DateTime date = DateTime.now();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    startTimer();
+  }
+
+  void startTimer() {
+    timer = Timer.periodic(Duration(seconds: 1), (_) {
+      setState(() => time--);
+      if (time == 0) {
+        Navigator.pop(context);
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       title: Container(
         alignment: Alignment.center,
-        child: Text("Mã thanh toán ${data}", style: AppText.appbarText2,),
+        child: Text("Mã thanh toán ${widget.data}", style: AppText.appbarText2,),
       ),
       titleTextStyle: AppText.alertText,
       elevation: 5,
       alignment: Alignment.center,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(AppNumber.h60)),
-      content: QrImage(
-        data: '${accountId} - ${data} - ${DateTime.now()}',
-        version: QrVersions.auto,
-        gapless: false,
-        size: AppSize.widthScreen * 0.7,
-      ),
+      content: Container(
+        height: AppSize.heightScreen / 2.7,
+        child: Column(
+          children: [
+            QrImage(
+              data: '${widget.accountId} - ${widget.data} - $date',
+              version: QrVersions.auto,
+              gapless: false,
+              size: AppSize.widthScreen * 0.7,
+            ),
+            Text('Mã sẽ biến mất sau $time giây', style: AppText.detailText2,),
+          ],
+        ),
+      )
     );
   }
 }
