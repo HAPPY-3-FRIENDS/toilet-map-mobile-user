515726e61826f24b06cdd8e21c2d0ef33959b9bd
qmint <nguyendaoducquan@gmail.com>
2023-05-10 07:52:24 +0700

    [dq-qiji] UI - Information - done UI for information main screen

--- lib/app/ui/information/information_main_screen.dart
+++ lib/app/ui/information/information_main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:toiletmap/app/repositories/user_info_repository.dart';
@@ -17,21 +18,38 @@ class InformationMainScreen extends StatelessWidget {
       top: true,
       bottom: true,
       child: Scaffold(
-          appBar: AppBar(
-            title: Text('Thông tin cá nhân'),
-            titleTextStyle: AppText.appbarTitleText1,
-            centerTitle: true,
-            toolbarHeight: AppSize.heightScreen / 10,
-            backgroundColor: Colors.transparent,
+          appBar: PreferredSize(
+            preferredSize: Size.fromHeight(100.h),
+            child: Container(
+              color: Colors.white,
+              padding: EdgeInsets.only(top: 20.h),
+              child: AppBar(
+                leading: IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: Icon(
+                    Icons.arrow_back_ios_rounded,
+                    color: AppColor.primaryColor1,
+                  ),
+                ),
 
-            flexibleSpace: Container(
-              height: AppSize.heightScreen / 10,
-              decoration: AppBoxDecoration.boxDecorationWithGradientNoBorder1,
+                title: Text('Thông tin cá nhân'),
+                titleTextStyle: AppText.appbarTitleText2,
+                centerTitle: true,
+                toolbarHeight: 100.h,
+                backgroundColor: Colors.white,
+                elevation: 0,
+
+                iconTheme: IconThemeData(
+                    color: AppColor.primaryColor1
+                ),
+              ),
             ),
           ),
 
           body: Container(
-            padding: EdgeInsets.all(AppSize.widthScreen / 30),
+            color: Color(0xFFF1F1F1),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
@@ -50,115 +68,166 @@ class InformationMainScreen extends StatelessWidget {
                         return Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Expanded(
-                                  flex: 1,
-                                  child: (snapshot!.data!.avatar != null)
-                                      ? CircleAvatar(
-                                      radius: AppSize.widthScreen / 7,
-                                      backgroundImage: NetworkImage(snapshot!.data!.avatar!)
-                                  )
-                                      : CircleAvatar(
-                                    radius: AppSize.widthScreen / 7,
-                                    backgroundImage: AssetImage('assets/default-avatar.png'),
+                            Container(
+                              color: Colors.white,
+                              height: 130.h,
+                              padding: EdgeInsets.all(10.w),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                children: [
+                                  Expanded(
+                                      flex: 1,
+                                      child: (snapshot!.data!.avatar != null)
+                                          ? Container(
+                                        padding: EdgeInsets.only(left: 10.w),
+                                        child: CircleAvatar(
+                                          radius: 50.w,
+                                          backgroundImage: NetworkImage(snapshot!.data!.avatar!),
+                                        ),
+                                      )
+                                          : Container(
+                                        padding: EdgeInsets.only(left: 10.w),
+                                        child: CircleAvatar(
+                                          radius: 50.w,
+                                          backgroundImage: AssetImage('assets/default-avatar.png'),
+                                        ),
+                                      )
                                   ),
-                                ),
-                                VerticalDivider(
-                                  color: AppColor.primaryColor1,
-                                  thickness: 1,
-                                  indent: AppSize.widthScreen / 35,
-                                  endIndent: AppSize.widthScreen / 35,
-                                ),
-                                Expanded(
-                                    flex: 2,
-                                    child: Column(
-                                      mainAxisAlignment: MainAxisAlignment.start,
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      children: [
-                                        Text(snapshot!.data!.fullName!, style: AppText.titleText2,),
-                                        Text('**********', style: AppText.titleText2,),
-                                      ],
-                                    )),
-                                VerticalDivider(
-                                  color: AppColor.primaryColor1,
-                                  thickness: 1,
-                                  indent: AppSize.widthScreen / 35,
-                                  endIndent: AppSize.widthScreen / 35,
-                                ),
-                                Expanded(
-                                  flex: 1,
-                                  child: InkWell(
-                                    onTap: () => showDialog(
-                                        context: context,
-                                        builder: (_) => AlertDialog(
-                                            elevation: 5,
-                                            alignment: Alignment.center,
-                                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(AppNumber.h60)),
-                                            content: Container(
-                                              height: AppSize.widthScreen * 0.8,
-                                              child: Column(
-                                                children: [
-                                                  QrImage(
-                                                    data: '23',
-                                                    version: QrVersions.auto,
-                                                    gapless: false,
-                                                    size: AppSize.widthScreen * 0.7,
-                                                  ),
-                                                  Text('Mã cá nhân', style: AppText.titleText2,),
-                                                ],
-                                              ),
-                                            )
-                                        )),
-                                    child:  QrImage(
-                                      data: '23',
-                                      version: QrVersions.auto,
-                                      gapless: false,
+                                  SizedBox(width: 20.w,),
+                                  Expanded(
+                                      flex: 2,
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                          Expanded(flex: 5,
+                                              child: Align(
+                                                alignment: Alignment.bottomLeft,
+                                                child: Text(snapshot!.data!.fullName!, style: AppText.titleText2,
+                                                  maxLines: 1, overflow: TextOverflow.ellipsis,),
+                                              )
+                                          ),
+                                          Expanded(flex: 1, child: Container(),),
+                                          Expanded(flex: 5,
+                                              child: Align(
+                                                alignment: Alignment.topLeft,
+                                                child: Text('**********', style: AppText.titleText2),
+                                              )
+                                          ),
+                                        ],
+                                      )),
+                                  VerticalDivider(
+                                    color: AppColor.primaryColor2,
+                                    thickness: 1,
+                                    indent: 15.w,
+                                    endIndent: 15.w,
+                                  ),
+                                  Expanded(
+                                    flex: 1,
+                                    child: InkWell(
+                                      onTap: () => showDialog(
+                                          context: context,
+                                          builder: (_) => AlertDialog(
+                                              elevation: 5,
+                                              alignment: Alignment.center,
+                                              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(AppNumber.h60)),
+                                              content: Container(
+                                                height: 300.h,
+                                                child: Column(
+                                                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                                  children: [
+                                                    QrImage(
+                                                      data: '23',
+                                                      version: QrVersions.auto,
+                                                      gapless: false,
+                                                      size: 200.w,
+                                                    ),
+                                                    Text('Mã cá nhân', style: AppText.titleText2,),
+                                                  ],
+                                                ),
+                                              )
+                                          )),
+                                      child:  QrImage(
+                                        size: 70.w,
+                                        data: '23',
+                                        version: QrVersions.auto,
+                                        gapless: false,
+                                      ),
                                     ),
                                   ),
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
-                            Divider(
-                              color: AppColor.primaryColor2,
-                              thickness: 1,
-                              indent: AppSize.widthScreen / 35,
-                              endIndent: AppSize.widthScreen / 35,
-                            ),
-                            Container(
-                              padding: EdgeInsets.all(AppSize.widthScreen / 30),
-                              child: Text("Thiết lập thông tin cá nhân"),
-                            ),
-                            Divider(
-                              color: AppColor.primaryColor2,
-                              thickness: 1,
-                              indent: AppSize.widthScreen / 35,
-                              endIndent: AppSize.widthScreen / 35,
-                            ),
-                            Container(
-                              padding: EdgeInsets.all(AppSize.widthScreen / 30),
-                              child: Text("Lịch sử"),
-                            ),
-                            Divider(
-                              color: AppColor.primaryColor2,
-                              thickness: 1,
-                              indent: AppSize.widthScreen / 35,
-                              endIndent: AppSize.widthScreen / 35,
+                            SingleChildScrollView(
+                              child: Column(
+                                children: [
+                                  SizedBox(
+                                    height: 5.h,
+                                  ),
+                                  Container(
+                                    height: 60.h,
+                                    width: double.infinity,
+                                    color: Colors.white,
+                                    padding: EdgeInsets.symmetric(horizontal: 15.w),
+                                    child: Align(
+                                        alignment: Alignment.centerLeft,
+                                        child: Row(
+                                          children: [
+                                            Row(
+                                              children: [
+                                                Icon(Icons.person, color: AppColor.primaryColor1, size: 20.w,),
+                                                SizedBox(width: 10.w),
+                                                Text("Thiết lập thông tin cá nhân", style: AppText.titleText2,),
+                                              ],
+                                            ),
+                                            Icon(Icons.arrow_forward_ios_outlined, size: 17, color: Colors.black38,),
+                                          ],
+                                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                        )
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 5.h,
+                                  ),
+                                  Container(
+                                    height: 60.h,
+                                    width: double.infinity,
+                                    color: Colors.white,
+                                    padding: EdgeInsets.symmetric(horizontal: 15.w),
+                                    child: Align(
+                                        alignment: Alignment.centerLeft,
+                                        child: Row(
+                                          children: [
+                                            Row(
+                                              children: [
+                                                Icon(Icons.history, color: AppColor.primaryColor1, size: 20.w,),
+                                                SizedBox(width: 10.w),
+                                                Text("Lịch sử", style: AppText.titleText2,),
+                                              ],
+                                            ),
+                                            Icon(Icons.arrow_forward_ios_outlined, size: 17, color: Colors.black38,),
+                                          ],
+                                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                        )
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ),
                           ],
                         );
                       }
                       return Center(child: Text('Lỗi'));
                     }),
-                SizedBox(
+                Container(
+                  padding: EdgeInsets.all(15.w),
                   width: double.infinity,
-                  height: AppNumber.h15,
+                  height: 90.h,
                   child: ElevatedButton(
                       style: ElevatedButton.styleFrom(
+                          elevation: 0,
                           primary: AppColor.primaryColor2,
                           shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(AppSize.widthScreen /20))),
+                              borderRadius: BorderRadius.circular(10.r))),
                       onPressed: () async {
                         try {
                           final prefs = await SharedPreferences.getInstance();
@@ -171,12 +240,12 @@ class InformationMainScreen extends StatelessWidget {
                           print(error);
                         }
                       },
-                      child: Text("Đăng xuất", style: TextStyle(color: Colors.black45),)
+                      child: Text("Đăng xuất", style: TextStyle(color: Colors.black),)
                   ),
                 )
               ],
             ),
-          ),
+          )
       ),
     );
   }
